e3440dda801ffef2e617e49dc5d0d42546794ed8
Brody Roche <brodyroche@yahoo.com>
2025-10-29 13:33:29 -0500

    Added loop for LetterGrade to ensure multiple chances to input valid value

--- HelloJava/src/main/java/LetterGrade.java
+++ HelloJava/src/main/java/LetterGrade.java
@@ -3,17 +3,24 @@ import java.util.Scanner;
 public class LetterGrade {
     public static void main(String[] arge){
         Scanner in = new Scanner(System.in);
-        double grade = 0;
+        double grade = -1;
+        boolean invalidInput = true;
         IO.print("Enter your numerical grade >>> ");
-        try{
-            grade = in.nextDouble();
-        } catch (Exception e){
-            IO.println("ERROR: Invalid Input");
-            System.exit(1);
+
+        while (invalidInput) {
+            try {
+                grade = in.nextDouble();
+
+                if (grade < 0 || grade > 100){
+                    IO.println("That value isn't in range from 0 - 100. Try again >>> ");
+                } else { invalidInput = false; }
+            } catch (Exception e) {
+                IO.println("ERROR: Invalid Input. Try again >>> ");
+                in.nextLine();
+            }
         }
 
-        if (grade < 0 || grade > 100) { IO.println("That value isn't in range from 0 - 100"); }
-        else if (grade < 60) { IO.println("F"); }
+        if (grade < 60) { IO.println("F"); }
         else if (grade < 70) { IO.println("D"); }
         else if (grade < 80) { IO.println("C"); }
         else if (grade < 90) { IO.println("B"); }
