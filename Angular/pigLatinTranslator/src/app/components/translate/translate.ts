90e98c2196978f37ea66c0b711381573000c6d22
Brody Roche <brodyroche@yahoo.com>
2025-11-25 11:36:41 -0600

    Finished PigLatin and created pokemon

--- Angular/pigLatinTranslator/src/app/components/translate/translate.ts
+++ Angular/pigLatinTranslator/src/app/components/translate/translate.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TranslatorService } from '../../services/translator-service';
 
 @Component({
   selector: 'app-translate',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './translate.html',
   styleUrl: './translate.css',
 })
 export class Translate {
+constructor(public translatorService:TranslatorService){}
 
+  englishText:string = ""
+  pigLatinText:string = ""
+
+  translate(){
+    if(!this.englishText){
+      alert("Please type something before translating!")
+      return
+    }
+    const words = this.englishText.split(/\b/);
+    const translatedWords = words.map(word => {
+      if (/^[a-zA-Z]+$/.test(word)) {
+        if (/^[aeiouAEIOU]/.test(word)) {
+          return word + 'way';
+        } else {
+          const match = word.match(/^([^aeiouAEIOU]+)(.*)$/);
+          return match ? match[2] + match[1] + 'ay' : word;
+        }
+      }
+      return word;
+    });
+    this.pigLatinText = translatedWords.join('');
+    this.translatorService.translationCounter++
+  }
 }
